1825685fb8802ba9dee1eb39817b09ea58fefb18
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-24 18:25:59 +0530

    Create string_object_creation.cpp

---
+++ Group1/string/string_object_creation.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int a=5;
+    string b="hello";
+    cout << a <<endl;
+    cout << b <<endl;
+
+    return 0;
+}
