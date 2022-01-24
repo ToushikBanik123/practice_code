25f401a78220864e74a5e7fbb73d7995a452c85a
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-24 18:40:17 +0530

    Create taking_input_in_string_object.cpp

---
+++ Group1/string/taking_input_in_string_object.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   
+    string b;
+   
+    cin >> b; // single word input
+    //getline(cin,b);
+    cout << b <<endl;
+    cin >> b; 
+    cout << b <<endl;
+
+    return 0;
+}
