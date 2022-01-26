440e7ffc9b446e106bf51539648363dc1ba27528
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-26 18:13:29 +0530

    Create capacity.cpp

---
+++ Group1/string/string_object/string_functions/capacity.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    //s.capacity will give the total length of the array
+    string s ="HelloWorld";
+  
+    cout<<s.size()<<endl;
+    cout<<s.capacity()<<endl;
+    
+
+    return 0;
+}
