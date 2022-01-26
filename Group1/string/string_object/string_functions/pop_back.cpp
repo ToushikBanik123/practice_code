58e4512f76c86150ac3ac41e213febc907231376
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-26 19:01:16 +0530

    Create pop_back.cpp

---
+++ Group1/string/string_object/string_functions/pop_back.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s="Programing";
+    // Insart a charactor (not a sting) at the end of the string
+    cout<<s<<endl;
+    s.pop_back();
+    
+    return 0;
+}
