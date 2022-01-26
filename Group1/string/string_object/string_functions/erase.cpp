db45a5b43fe649bd520381f80f9e383cc867a72f
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-26 18:52:30 +0530

    Create erase.cpp

---
+++ Group1/string/string_object/string_functions/erase.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s="Programing";
+    //This will erase the string just like s.clear()
+    s.erase();
+    cout<<s;
+    return 0;
+}
