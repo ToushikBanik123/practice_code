99cc224c6588db4493dcf685a0bf80085e166157
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-26 18:43:15 +0530

    Update append.cpp

--- Group1/string/string_object/string_functions/append.cpp
+++ Group1/string/string_object/string_functions/append.cpp
@@ -11,3 +11,25 @@ int main()
     cout<<s;
     return 0;
 }
+
+
+
+
+/*
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    
+    string s ="Hello";
+    cout<<s.capacity()<<endl;
+    s.append(" worldasjklanldanlndoanodnaonsonlanldnclnalncl");
+    cout<<s<<endl;
+     cout<<s.capacity()<<endl;
+    return 0;
+}
+
+*/
