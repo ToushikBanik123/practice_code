61509b18815a3fc55c5c61322454f863fe36dcc2
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-26 18:55:36 +0530

    Create pushback.cpp

---
+++ Group1/string/string_object/string_functions/pushback.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s="Programing";
+    // Insart a charactor (not a sting) at the end of the string
+    s.push_back('L');
+    cout<<s;
+    return 0;
+}
