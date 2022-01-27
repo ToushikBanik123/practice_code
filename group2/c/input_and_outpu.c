3e12befb0a3f71bf1696190487c67397ae63975c
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-27 19:15:00 +0530

    Create input_and_outpu.c

---
+++ group2/c/input_and_outpu.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    //printf("%d \n",a);
+    printf("hi I am %d year old and ram is %d year old",a,b); // cout <<"hi i am "<< a << "year old";
+
+    return 0;
+}
