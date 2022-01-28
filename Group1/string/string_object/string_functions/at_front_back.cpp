036446cb571eaafaefcc5855a9bf2d293ae5cd26
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-28 12:47:33 +0530

    Create at_front_back.cpp

---
+++ Group1/string/string_object/string_functions/at_front_back.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s ="chakraborty";
+    cout<<s.at(3)<<endl;
+    cout << s.front()<<endl;
+    cout << s.back()<<endl;
+
+
+    return 0;
+}
